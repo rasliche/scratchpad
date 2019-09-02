60da92e260c488abd7a64f593e64d3331f500890
Crispin <crispinkoech.ck@gmail.com>
2019-09-02 21:11:28 +0300

    Regular task tab.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,10 +9,14 @@
           <li class="nav-item mr-2">
             <a @click="show_timers_tab" href="javascript:void(0)" :class="activeTab === 'timers_tab' ? 'nav-link active' : 'nav-link'">My timers ({{timers.length}})</a>
           </li>
+          <li class="nav-item mr-2">
+            <a @click="show_regulars_tab" href="javascript:void(0)" :class="activeTab === 'regulars_tab' ? 'nav-link active' : 'nav-link'">My Regular Tasks ({{tasksCount}})</a>
+          </li>
           <Tab v-for="column in columns" :key="column.id" :column="column" />
         </ul>
         <Scratchpad v-if="active_column" :column="active_column" />
         <Timerspad v-if="activeTab === 'timers_tab'" />
+        <RegularTasksPad v-if="activeTab === 'regulars_tab'" />
       </div>
     </div>
   </div>
@@ -21,13 +25,15 @@
 <script>
 import Timerspad from '../components/pads/Timerspad'
 import Scratchpad from '../components/pads/Scratchpad'
+import RegularTasksPad from '../components/pads/RegularTasksPad'
 import Tab from '../components/Tab'
 
-import { mapState } from 'vuex';
+import { mapState, mapGetters } from 'vuex';
 
 export default  {
   name:'Home',
   components:{
+    RegularTasksPad,
     Scratchpad,
     Timerspad,
     Tab
@@ -40,11 +46,17 @@ export default  {
       columns: state => state.columns,
       timers: state => state.timer.timers,
       activeTab: state => state.activeTab
-    })
+    }),
+    ...mapGetters({
+      tasksCount: 'regularTasks/tasksCount',
+    }),
   },
   methods:{
     show_timers_tab(){
       this.$store.dispatch('update_active_tab', 'timers_tab')
+    },
+    show_regulars_tab(){
+      this.$store.dispatch('update_active_tab', 'regulars_tab')
     },
     add_new_column(){
       this.$store.dispatch('add_new_column', {title: 'Change me'})
